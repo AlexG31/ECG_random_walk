f05104d84824f9fc05329a72717e25c0084e7f83
lab-ubuntu <pengfeigaothu@gmail.com>
2017-02-27 18:13:22 +0800

    Testing

---
+++ testing/test_api.py
@@ -0,0 +1,135 @@
+#encoding:utf8
+import os
+import sys
+import time
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+import pdb
+
+from dpi.DPI_QRS_Detector import DPI_QRS_Detector as DPI
+from QTdata.loadQTdata import QTloader
+from random_walker import RandomWalker
+
+
+def Testing(raw_sig, fs, model_list):
+    '''Testing API.
+    Input:
+        raw_sig: ECG signal.
+        model_list: random walker models and their biases relative to QRS.
+    Output:
+        List of (pos, label) pairs.
+    '''
+    dpi = DPI(debug_info = dict())
+    r_list = dpi.QRS_Detection(raw_sig, fs = fs)
+    walk_results = Testing_random_walk(raw_sig, fs, r_list, model_list)
+    walk_results.extend(zip(r_list, ['R',] * len(r_list)))
+    return walk_results
+
+def Testing_random_walk(raw_sig, fs, qrs_locations, model_list):
+    '''
+    Testing with random walk based on QRS locations.
+    Inputs:
+        raw_sig: ECG input.
+        qrs_locations: indexes of QRS locations.
+        model_list: random walker models and their biases relative to QRS.
+    Output:
+        List of (pos, label) pairs.
+    '''
+    testing_results = list()
+
+    # For benchmarking
+    Tnew_list = list()
+    walker_time_cost = 0
+    # Maybe batch walk?
+    feature_extractor = None
+    for R_pos in qrs_locations:
+        for walker, bias in model_list:
+            bias = int(float(fs) * bias)
+
+            start_time = time.time()
+            if feature_extractor is None:
+                feature_extractor = walker.GetFeatureExtractor(raw_sig)
+            results = walker.testing_walk_extractor(feature_extractor, R_pos + bias,
+                    iterations = 100,
+                    stepsize = 10)
+            # results = walker.testing_walk(raw_sig, R_pos + bias,
+                    # iterations = 100,
+                    # stepsize = 10)
+            # print 'testing time cost: %f s.' % (time.time() - start_time)
+            walker_time_cost += time.time() - start_time
+
+            path, probability = zip(*results)
+            Tnew_list.append(len(set(path)))
+            predict_position = int(np.mean(path[len(path) / 2:]))
+            testing_results.append((predict_position,
+                    walker.target_label))
+            
+    print 'Walker time cost %f seconds.' % walker_time_cost
+    print 'Average number of new samples to test: %f.' % np.mean(Tnew_list)
+    return testing_results
+
+
+
+def GetModels():
+    '''Returns model dict.'''
+    label_list = ['P', 'Ponset', 'Poffset',
+            'T',]
+    # label_list = ['P', 'Ponset', 'Poffset',
+            # 'T', 'Toffset',
+            # 'Ronset', 'R', 'Roffset']
+    bias_list = [
+                -0.19, -0.19, -0.19,
+                0.27, 0.2,
+                -0.02, 0, 0.02,
+            ]
+    # Get model dict
+    model_folder = '/home/alex/LabGit/ECG_random_walk/training/data/m4_models'
+    models = list()
+    for target_label, bias in zip(label_list, bias_list):
+        model_file_name = os.path.join(model_folder, target_label + '.mdl')
+        walker = RandomWalker(target_label = target_label)
+        walker.load_model(model_file_name)
+        models.append((walker, bias))
+    return models
+
+
+
+def Test1():
+    '''Test case1.'''
+    fs = 250.0
+    qt = QTloader()
+    sig = qt.load('sel103')
+    raw_sig = sig['sig']
+    raw_sig = raw_sig[0:250 * 120]
+
+    model_list = GetModels()
+    start_time = time.time()
+    # results = Testing_random_walk(raw_sig, 250.0, r_list, model_list)
+    results = Testing(raw_sig, 250.0, model_list)
+    print 'Testing time cost %f secs.' % (time.time() - start_time)
+
+    samples_count = len(raw_sig)
+    time_span = samples_count / fs
+    print 'Span of testing range: %f samples(%f seconds).' % (samples_count, time_span)
+
+    # Display results
+    plt.figure(1)
+    plt.plot(raw_sig, label = 'ECG')
+    pos_list, label_list = zip(*results)
+    labels = set(label_list)
+    for label in labels:
+        pos_list = [int(x[0]) for x in results if x[1] == label]
+        amp_list = [raw_sig[x] for x in pos_list]
+        plt.plot(pos_list, amp_list, 'o',
+                markersize = 15,
+                label = label)
+    plt.title('sel32')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    Test1()
+
---
+++ training/qt_full_test.py
@@ -0,0 +1,176 @@
+#encoding:utf8
+import os
+import sys
+import matplotlib.pyplot as plt
+import pdb
+import time
+import json
+import random
+import numpy as np
+
+from random_walker import RandomWalker
+from QTdata.loadQTdata import QTloader
+
+
+def Test1(target_label = 'P', num_training = 25):
+    '''Test case 1: random walk.'''
+    qt = QTloader()
+    record_list = qt.getreclist()
+    training_list = random.sample(record_list, num_training)
+    testing_list = list(set(record_list) - set(training_list))
+
+    random_forest_config = dict(
+            max_depth = 10)
+    walker = RandomWalker(target_label = target_label,
+            random_forest_config = random_forest_config)
+
+    start_time = time.time()
+    for record_name in training_list:
+        print 'Collecting features from record %s.' % record_name
+        sig = qt.load(record_name)
+        walker.collect_training_data(sig['sig'], qt.getExpert(record_name))
+    print 'random forest start training...'
+    walker.training()
+    print 'trianing used %.3f seconds' % (time.time() - start_time)
+
+    for record_name in testing_list:
+        sig = qt.load(record_name)
+        raw_sig = sig['sig']
+
+        seed_position = random.randint(100, len(raw_sig) - 100)
+        plt.figure(1)
+        plt.clf()
+        plt.plot(sig['sig'], label = record_name)
+        plt.title(target_label)
+        for ti in xrange(0, 20):
+            seed_position += random.randint(1,200)
+            print 'testing...(position: %d)' % seed_position
+            start_time = time.time()
+            results = walker.testing_walk(sig['sig'], seed_position, iterations = 100,
+                    stepsize = 10)
+            print 'testing finished in %.3f seconds.' % (time.time() - start_time)
+
+            pos_list, values = zip(*results)
+            predict_pos = np.mean(pos_list[len(pos_list) / 2:])
+            
+            # amp_list = [raw_sig[int(x)] for x in pos_list]
+            amp_list = []
+            bias = raw_sig[pos_list[0]]
+            for pos in pos_list:
+                amp_list.append(bias)
+                bias -= 0.01
+
+            plt.plot(predict_pos,
+                    raw_sig[int(predict_pos)],
+                    'ro',
+                    markersize = 14,
+                    label = 'predict position')
+            plt.plot(pos_list, amp_list, 'r',
+                    label = 'walk path',
+                    markersize = 3,
+                    linewidth = 8,
+                    alpha = 0.3)
+            plt.xlim(min(pos_list) - 100, max(pos_list) + 100)
+            plt.grid(True)
+            plt.legend()
+            plt.show(block = False)
+            pdb.set_trace()
+
+def testing(random_walker, raw_sig, seed_step_size = 200):
+    result_list = list()
+    start_time = time.time()
+    for seed_position in xrange(0, len(raw_sig), seed_step_size):
+        sys.stdout.write('\rTesting: %06d samples left.' % (len(raw_sig) - 1 - seed_position))
+        sys.stdout.flush()
+
+        results = random_walker.testing_walk(raw_sig,
+                seed_position,
+                iterations = 100,
+                stepsize = 10)
+        pos_list, values = zip(*results)
+        predict_pos = np.mean(pos_list[len(pos_list) / 2:])
+        confidence = 1.0
+        result_list.append((predict_pos, random_walker.target_label, confidence, pos_list))
+    print 'testing finished in %.3f seconds.' % (time.time() - start_time)
+    return result_list
+
+def RoundTest(target_label, result_folder, num_training = 75):
+    '''Randomly select num_training records to train, and test others.'''
+    qt = QTloader()
+    record_list = qt.getreclist()
+    must_train_list = [
+        "sel35", 
+        "sel36", 
+        "sel31", 
+        "sel38", 
+        "sel39", 
+        "sel820", 
+        "sel51", 
+        "sele0104", 
+        "sele0107", 
+        "sel223", 
+        "sele0607", 
+        "sel102", 
+        "sele0409", 
+        "sel41", 
+        "sel40", 
+        "sel43", 
+        "sel42", 
+        "sel45", 
+        "sel48", 
+        "sele0133", 
+        "sele0116", 
+        "sel14172", 
+        "sele0111", 
+        "sel213", 
+        "sel14157", 
+        "sel301"
+            ]
+    num_training -= len(must_train_list)
+    record_list = list(set(record_list) - set(must_train_list))
+    training_list = must_train_list
+    if num_training > 0:
+        training_list.extend(random.sample(record_list, num_training))
+    testing_list = list(set(record_list) - set(training_list))
+
+    random_forest_config = dict(
+            max_depth = 10)
+    walker = RandomWalker(target_label = target_label,
+            random_forest_config = random_forest_config)
+
+    start_time = time.time()
+    for record_name in training_list:
+        print 'Collecting features from record %s.' % record_name
+        sig = qt.load(record_name)
+        walker.collect_training_data(sig['sig'], qt.getExpert(record_name))
+    print 'random forest start training...'
+    walker.training()
+    print 'trianing used %.3f seconds' % (time.time() - start_time)
+
+    for record_name in testing_list:
+        print 'testing record %s...' % record_name
+        record_result = list()
+        sig = qt.load(record_name)
+        raw_sig = sig['sig']
+        record_result.append((record_name, testing(walker, raw_sig)))
+        raw_sig = sig['sig2']
+        record_result.append((record_name + '_sig2', testing(walker, raw_sig)))
+        # Write to json
+        with open(os.path.join(result_folder, '%s.json' % record_name), 'w') as fout:
+            json.dump(record_result, fout, indent = 4)
+
+
+
+
+
+if __name__ == '__main__':
+    # Test1()
+    label_list = ['P', 'Ponset', 'Poffset',
+            'T', 'Toffset',
+            'Ronset', 'R', 'Roffset']
+    root_folder = 'data/test_results/r1'
+    for target_label in label_list:
+        result_folder = os.path.join(root_folder, target_label)
+        if os.path.exists(result_folder) == False:
+            os.mkdir(result_folder)
+        RoundTest(target_label, result_folder)
