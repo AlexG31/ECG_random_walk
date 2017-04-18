087bfd111abd8afd48f83a04969aa9f5b6c6da3b
lab-ubuntu <pengfeigaothu@gmail.com>
2017-04-18 19:51:52 +0800

    add test cases

--- randomwalk/test_api.py
+++ randomwalk/test_api.py
@@ -12,6 +12,58 @@ from dpi.DPI_QRS_Detector import DPI_QRS_Detector as DPI
 from random_walker import RandomWalker
 
 
+curfilepath =  os.path.realpath(__file__)
+current_folderpath = os.path.dirname(curfilepath)
+
+def Denoise(raw_sig, level = 7, low = 1, high = 1):
+    '''Denoise with wavelet.'''
+    def crop_data_for_swt(rawsig):
+        '''Padding zeros to make the length of the signal to 2^N.'''
+        import numpy as np
+        if isinstance(rawsig, np.ndarray):
+            rawsig = rawsig.tolist()
+        # crop rawsig
+        base2 = 1
+        N_data = len(rawsig)
+        if len(rawsig)<=1:
+            raise Exception('len(rawsig)={}, not enough for swt!', len(rawsig))
+        crop_len = base2
+        while base2<N_data:
+            if base2*2>=N_data:
+                crop_len = base2*2
+                break
+            base2*=2
+        # Extending this signal input with its tail element.
+        if N_data< crop_len:
+            rawsig += [rawsig[-1],]*(crop_len-N_data)
+        return rawsig
+
+    import pywt
+    len_sig = len(raw_sig)
+    raw_sig = crop_data_for_swt(raw_sig)
+    coeflist = pywt.swt(raw_sig, 'db2', level)
+    cAlist, cDlist = zip(*coeflist)
+
+    cAlist = list(cAlist)
+    cDlist = list(cDlist)
+
+    # denoise
+    for ind in xrange(0, low):
+        cAlist[ind] = np.zeros(len(cAlist[ind]))
+    for ind in xrange(len(cDlist) - high, len(cDlist)):
+        cDlist[ind] = np.zeros(len(cDlist[ind]))
+    
+    coeflist = zip(cAlist, cDlist)
+    denoised_sig = pywt.iswt(coeflist, 'db2')
+    
+    # plt.figure(1)
+    # plt.plot(denoised_sig)
+    # plt.plot(sig, 'r')
+    # plt.show()
+
+    raw_sig = denoised_sig[:len_sig]
+    print len_sig
+    return raw_sig
 
 def Testing(raw_sig_in, fs, model_list, walker_iterations = 100, walker_stepsize = 10):
     '''Testing API.
@@ -582,6 +634,8 @@ def GetModels(model_folder, pattern_file_name):
     label_list = ['P', 'Ponset', 'Poffset',
             'T', 'Toffset', 'Tonset',
             'Ronset', 'Roffset']
+    # label_list = ['P', 'Ponset', 'Poffset']
+    # label_list = ['P', ]
     bias_list = [
                 -0.19, -0.195, -0.185,
                 0.26, 0.27, 0.1,
@@ -615,7 +669,7 @@ def Test1():
     model_list = GetModels(model_folder, pattern_file_name)
     start_time = time.time()
     # results = Testing_random_walk(raw_sig, 250.0, r_list, model_list)
-    results = Testing(raw_sig, 250.0, model_list)
+    results = Testing(raw_sig, 250.0, model_list, walker_iterations = 100)
     print 'Testing time cost %f secs.' % (time.time() - start_time)
 
     samples_count = len(raw_sig)
@@ -638,6 +692,138 @@ def Test1():
     plt.legend()
     plt.show()
 
+def TestChanggeng(record_ind):
+    '''Test case1.'''
+    def RunWalkerModel(walker_model, seed_positions, confined_ranges, feature_extractor):
+        '''Run random walk detection model.
+        Input:
+            walker_model: random walk regressor for a certain label.
+            seed_positions: list of seed position
+            confined_ranges: list of confined_range
+        '''
+        if abs(fs - 250.0) > 1e-6:
+            raise Exception('Bias has default fs = 250.0Hz!')
+
+        print 'fs = ', fs
+
+        # First add to prepare testing list
+        for seed_position, confined_range in zip(seed_positions, confined_ranges):
+            walker_model.prepareTestSample(seed_position, confined_range)
+
+        start_time = time.time()
+
+        # Second, Testing all prepared positions
+        path_list, scores_list = walker_model.runPreparedTesting(feature_extractor, iterations = 200, stepsize = 4)
+
+        results = list()
+        for path in path_list:
+            # Tnew_list.append(len(set(path)))
+            predict_position = int(np.mean(path[len(path) / 2:]) / 250.0 * fs)
+
+            # For return value of super function
+            results.append((predict_position,
+                    walker_model.target_label))
+        return (results, path_list)
+
+    import matplotlib.pyplot as plt
+    import random
+    fs = 250.0
+    from QTdata.loadQTdata import QTloader
+    from changgengLoader import ECGLoader
+
+    ecg = ECGLoader(500, current_folderpath)
+    record_name = ecg.P_faillist[record_ind]
+    sig = ecg.load(record_name)
+    raw_sig = sig[0]
+    import scipy.signal
+    # raw_sig = Denoise(raw_sig)
+    resampled_sig = scipy.signal.resample_poly(raw_sig, 1, 2)
+    # plt.figure(1)
+    # plt.plot(raw_sig, label = 'signal')
+    # plt.plot(xrange(0, len(raw_sig), 2), resampled_sig, label = 'resmaple')
+    # plt.legend()
+    # plt.grid(True)
+    # plt.title(record_name)
+    # plt.show()
+
+    raw_sig = resampled_sig
+
+    model_folder = '/home/alex/LabGit/ECG_random_walk/randomwalk/data/Lw3Np4000/improved'
+    pattern_file_name = '/home/alex/LabGit/ECG_random_walk/randomwalk/data/Lw3Np4000/random_pattern.json'
+    model_list = GetModels(model_folder, pattern_file_name)
+    start_time = time.time()
+
+    # Start Testing
+    results = list()
+    # results = Testing_random_walk(raw_sig, 250.0, r_list, model_list)
+    # results = Testing(raw_sig, 250.0, model_list, walker_iterations = 200)
+    feature_extractor = model_list[0][0].GetFeatureExtractor(raw_sig)
+    for walker_model, bias, model_label in model_list:
+        if model_label != 'P':
+            continue
+        print 'Testing model label:', model_label
+        seeds = list()
+        confined_ranges = list()
+        for pos in xrange(1, len(raw_sig), 200):
+            seeds.append(pos)
+            confined_ranges.append([0, len(raw_sig) - 1])
+        seed_results, path_list = RunWalkerModel(walker_model, seeds, confined_ranges, feature_extractor)
+        results.extend(seed_results)
+
+    print 'Testing time cost %f secs.' % (time.time() - start_time)
+
+    samples_count = len(raw_sig)
+    time_span = samples_count / fs
+    #print 'Span of testing range: %f samples(%f seconds).' % (samples_count, time_span)
+
+    # Display results
+    plt.figure(1)
+    plt.clf()
+    plt.plot(raw_sig, label = 'ECG')
+    pos_list, label_list = zip(*results)
+    labels = set(label_list)
+    for label in labels:
+        if label != 'P':
+            continue
+        pos_list = [int(x[0]) for x in results if x[1] == label]
+        amp_list = [raw_sig[x] for x in pos_list]
+        marker = 'o' if len(label) == 1 else '|'
+        plt.plot(pos_list, amp_list,
+                marker = marker,
+                linestyle = 'none',
+                markeredgewidth = 5,
+                markersize = 15,
+                alpha = 0.85,
+                markerfacecolor = 'none',
+                markeredgecolor = (random.random(),
+                                    random.random(),
+                                    random.random(),
+                    ),
+                label = label)
+
+        # Plot path
+        for path, up_amplitude in zip(path_list, amp_list):
+            plt.plot(path, xrange(up_amplitude, up_amplitude - int(len(path) * 0.01) + 1, 0.01),'r', alpha = 0.43)
+
+    # Plot failed test
+    fail_results = ecg.loadAnnot(record_name, target_label = 'P')
+    pos_list = [int(x[0] / 2) for x in fail_results if x[1] == 'P']
+    amp_list = [raw_sig[x] for x in pos_list]
+    plt.plot(pos_list, amp_list, 'x',
+            markersize = 15,
+            markeredgewidth = 5,
+            alpha = 0.5,
+            label = 'failed')
+    
+
+    plt.title(record_name)
+    plt.grid(True)
+    plt.legend()
+    plt.show(block = False)
+    pdb.set_trace()
+
 if __name__ == '__main__':
-    Test1()
+    # Test1()
+    for record_ind in xrange(0, 12):
+        TestChanggeng(record_ind)
 
